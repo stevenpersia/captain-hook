9d079f8726f479729fd40e1a91a005c543ac0228
Steven Persia <persia.steven8@gmail.com>
2020-03-25 10:43:03 +0100

    Fix useKeyPress

--- useKeyPress.js
+++ useKeyPress.js
@@ -1,22 +1,27 @@
 import { useEffect, useState } from "react";
 
 const useKeyPress = targetKey => {
-	const [keyPressed, setKeyPressed] = useState(false);
+  const [keyPressed, setKeyPressed] = useState(false);
 
-	const downHandler = key => key === targetKey && setKeyPressed(true);
-	const upHandler = key => key === targetKey && setKeyPressed(false);
+  const downHandler = ({ key }) => {
+    if (key === targetKey) setKeyPressed(true);
+  };
 
-	useEffect(() => {
-		window.addEventListener("keydown", downHandler);
-		window.addEventListener("keyup", upHandler);
+  const upHandler = ({ key }) => {
+    if (key === targetKey) setKeyPressed(false);
+  };
 
-		return () => {
-			window.removeEventListener("keydown", downHandler);
-			window.removeEventListener("keyup", upHandler);
-		};
-	}, []);
+  useEffect(() => {
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
 
-	return keyPressed;
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, []);
+
+  return keyPressed;
 };
-
 export default useKeyPress;
+
