68d292adddda0031292bb0908404d1eea4a297df
Steven Persia <persia.steven8@gmail.com>
2020-03-25 10:58:30 +0100

    Update useSlug.js

--- useSlug.js
+++ useSlug.js
@@ -6,13 +6,13 @@ const useSlug = string => {
 	return string
 		.toString()
 		.toLowerCase()
-		.replace(/\s+/g, "-") // Replace spaces with -
-		.replace(regex, character => to.charAt(from.indexOf(character))) // Replace special characters
-		.replace(/&/g, "-and-") // Replace & with ‘and’
-		.replace(/[^\w\-]+/g, "") // Remove all non-word characters
-		.replace(/\-\-+/g, "-") // Replace multiple - with single -
-		.replace(/^-+/, "") // Trim - from start of text
-		.replace(/-+$/, ""); // Trim - from end of text
+		.replace(/\s+/g, "-")
+		.replace(regex, character => to.charAt(from.indexOf(character)))
+		.replace(/&/g, "-and-")
+		.replace(/[^\w\-]+/g, "")
+		.replace(/\-\-+/g, "-")
+		.replace(/^-+/, "")
+		.replace(/-+$/, "");
 };
 
 export default useSlug;
