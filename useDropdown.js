cf2f7a11889969eee2ea0063bf47d3802f9c2b18
Ahsan-Ehtesham <ahsanehtesham110@gmail.com>
2021-02-21 20:57:44 +0500

    Added Dropdown Basic Custom Hook

---
+++ useDropdown.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const useDropdown = (label, defaultState, options) => {
+  const [state, setState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
+  const Dropdown = () => (
+    <label htmlFor={id}>
+      {label}
+      <select
+        id={id}
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+        onBlur={(e) => setState(e.target.value)}
+        disabled={options.length === 0}
+      >
+        <option>All</option>
+        {options.map((item) => (
+          <option key={item} value={item}></option>
+        ))}
+      </select>
+    </label>
+  );
+
+  return [state, Dropdown, setState];
+};
+
+export default useDropdown;
+
+
+
